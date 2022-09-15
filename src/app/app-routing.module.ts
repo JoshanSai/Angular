21aa7d4ef0e1b605ab60a5baa4fac4dbe408bb6b
Joshan Dasireddy <joshan.dasi@argano.com>
2022-09-15 17:33:16 +0530

    allchange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes =[
+  {path:'',component: HomeComponent},
+  {path:'parent',component:ParentComponent},
+  {path:'child',component:ChildComponent}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
